b69e58ec88458fd03c556f752af6b62ec77d36f4
PabloVys <vysochynskyi@stud.uniza.sk>
2025-11-29 22:36:22 +0100

    added normal pdf viewer

---
+++ app/components/PdfComponent.client.vue
@@ -0,0 +1,11 @@
+<template>
+  <VuePDF
+    :pdf="pdf"
+    style="display: flex; justify-content: center; margin: 5px 0px"
+  />
+</template>
+<script setup lang="ts">
+import { VuePDF, usePDF } from "@tato30/vue-pdf";
+import "@tato30/vue-pdf/style.css";
+const { pdf } = usePDF("/Pavlo-Vysochynskyi-CV-ENG.pdf");
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,40 +1,11 @@
 <template>
   <div>
-    <fieldset
-      style="margin-top: 30px; margin-right: 2px; background-color: #c0c0c0"
-    >
+    <fieldset style="margin-top: 30px; background-color: #c0c0c0">
       <legend>My CV</legend>
-      <div v-if="loading">
-        <div class="progress-indicator segmented">
-          <span
-            class="progress-indicator-bar"
-            :style="{ width: progress + '%' }"
-          />
-        </div>
-      </div>
-
-      <iframe
-        src="/Pavlo-Vysochynskyi-CV-ENG.pdf"
-        width="100%"
-        height="700px"
-        v-show="!loading"
-        @load="loading = false"
-      />
+      <ClientOnly>
+        <PdfComponent />
+      </ClientOnly>
     </fieldset>
   </div>
 </template>
-<script setup lang="ts">
-const loading = ref(true);
-const progress = ref(0);
-
-onBeforeMount(() => {
-  const interval = setInterval(() => {
-    if (progress.value < 100) {
-      progress.value += 10;
-    } else {
-      clearInterval(interval);
-      loading.value = false;
-    }
-  }, 50);
-});
-</script>
+<script setup lang="ts"></script>
