5f205cfdd44a58d3e36f6572707c8c7b3e0c64c4
PabloVys <vysochynskyi@stud.uniza.sk>
2025-11-29 22:51:51 +0100

    fixed size of pdf

--- app/components/PdfComponent.client.vue
+++ app/components/PdfComponent.client.vue
@@ -2,13 +2,12 @@
   <VuePDF
     :pdf="pdf"
     style="display: flex; justify-content: center; margin: 5px 0px"
-    :width="width"
+    fit-parent
   />
 </template>
 <script setup lang="ts">
 import { VuePDF, usePDF } from "@tato30/vue-pdf";
 import "@tato30/vue-pdf/style.css";
 const { pdf } = usePDF("/Pavlo-Vysochynskyi-CV-ENG.pdf");
-const width = computed(() => window.innerWidth / 2);
 </script>
 <style scoped></style>
