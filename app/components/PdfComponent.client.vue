44242997e1e88895b16c5b8973bb1d9816635b2f
PabloVys <vysochynskyi@stud.uniza.sk>
2025-11-30 17:57:13 +0100

    small updates

--- app/components/PdfComponent.client.vue
+++ app/components/PdfComponent.client.vue
@@ -3,11 +3,20 @@
     :pdf="pdf"
     style="display: flex; justify-content: center; margin: 5px 0px"
     fit-parent
+    text-layer
+    annotation-layer
+    @annotation="onAnnotation"
   />
 </template>
 <script setup lang="ts">
 import { VuePDF, usePDF } from "@tato30/vue-pdf";
 import "@tato30/vue-pdf/style.css";
 const { pdf } = usePDF("/Pavlo-Vysochynskyi-CV-ENG.pdf");
+
+const onAnnotation = (value: any) => {
+  if (value.type == "link") {
+    window.open(value.data.url, "_blank");
+  }
+};
 </script>
 <style scoped></style>
